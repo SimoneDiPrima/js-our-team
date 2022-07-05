c278776ad7d29e453484d8da165b2b657f6817ce
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-07-05 14:22:38 +0200

    creazione array di oggetti e stampa in console delle informazioni per tipo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
 console.log(`js ok`);
 
+// prima di tutto devo creare un array:
 
+const personArray = [
+   {
+    "nome": "Wayne",
+    "surname": "Barnett",
+    "profession":"founder & ceo",
+    "image": "wayne-barnett-founder-ceo.jpg"
+   },
+   {
+    "nome": "Angela",
+    "surname": "Carroll",
+    "profession":"chief editor",
+    "image": "angela-caroll-chief-editor.jpg"
+   },
+   {
+    "nome": "walter",
+    "surname": "Gordon",
+    "profession":"Office Manager",
+    "image": "walter-gordon-office-manager.jpg"
+   },
+   {
+    "nome": "Angela",
+    "surname": "Lopez",
+    "profession":"Social Media Manager",
+    "image": "angela-lopez-social-media-manager.jpg"
+   },
+   {
+    "nome": "Scott",
+    "surname": "Estrada",
+    "profession":"Developer",
+    "image": "scott-estrada-developer.jpg"
+   },
+   {
+    "nome": "Barbara",
+    "surname": "Ramos",
+    "profession":"Graphic designer",
+    "image": "barbara-ramos-graphic-designer.jpg "
+   }
+]
+
+console.log(personArray);
